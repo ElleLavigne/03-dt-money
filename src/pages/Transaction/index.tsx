5b93b9785301eb5dc7e58a81fb80adeecbbb8d02
Izaelle Lavigne <izaelleavrillavigne@gmail.com>
2024-10-15 13:58:06 -0300

    feat transaction summary form

--- src/pages/Transaction/index.tsx
+++ src/pages/Transaction/index.tsx
@@ -1,11 +1,40 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../Summary'
+import { PriceHighLight, TransactionsContainer, TransactionTable } from './styles'
 
 export function Transaction(){
   return(
     <div>
       <Header/>
       <Summary/>
+       <TransactionsContainer>
+      <TransactionTable>
+         <tbody>
+          <tr>
+            <td width='50%'>Desenvolvimento de sites</td>
+            <td>
+              <PriceHighLight variant='income'>
+              R$12.000,00
+              </PriceHighLight>
+              </td>
+            <td>Venda</td>
+            <td>13/04/2022</td>
+          </tr>
+
+          <tr>
+            <td width='50%'>Hamburguer</td>
+            <td>
+             < PriceHighLight variant='outcome'>
+              - R$59,90
+             </PriceHighLight>
+              </td>
+            <td>Alimentação</td>
+            <td>10/04/2022</td>
+          </tr>
+          
+        </tbody>
+      </TransactionTable>
+       </TransactionsContainer>
     </div>
   )
 }
